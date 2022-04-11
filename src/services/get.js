442b4a92a4b6f1608fcc03d4e6d189fba3e9ede6
Pamela Elin <sosapamelaelin@gmail.com>
2022-04-11 18:19:33 -0300

    query

--- src/services/get.js
+++ src/services/get.js
@@ -1,6 +1,6 @@
-const API_URL =
-  "https://api.themoviedb.org/3/movie/popular?api_key=98300ceed5425e19dee70a74196442b3";
-
+const KEY_API='98300ceed5425e19dee70a74196442b3'
+const API_URL =`https://api.themoviedb.org/3/movie/popular?api_key=${KEY_API}`;
+const QUERY_URL=`https://api.themoviedb.org/3/search/multi?api_key=${KEY_API}`;
 export const getMovies = async () => {
   const res = await fetch(API_URL);
   const data = await res.json();
@@ -8,7 +8,7 @@ export const getMovies = async () => {
 };
 
 export const searchMovie = async (query) => {
-  const url = `https://api.themoviedb.org/3/search/multi?api_key=98300ceed5425e19dee70a74196442b3&query=${query}`;
+  const url = `${QUERY_URL}&query=${query}`;
   const res = await fetch(`${query ? url : API_URL}`);
   const data = await res.json();
   
